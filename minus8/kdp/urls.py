e506d2c28984b85283388555842b4f95ad6969dc
Chance Carmichael <horeth@oswego.edu>
2018-12-14 20:22:29 -0500

    added url routing to kdp

--- minus8/kdp/urls.py
+++ minus8/kdp/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-	path('kdp/', include('kdp.urls')),
-	path('admin/', admin.site.urls),
+	path('', views.index, name='index'),
 ]
